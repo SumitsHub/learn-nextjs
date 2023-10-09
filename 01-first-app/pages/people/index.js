f7cd68573f7272e74e78c45ee0c0df109c771159
Sumit Paul <pauls1317@gmail.com>
2023-10-09 22:41:51 +0530

    first-app: intro to file based routing

---
+++ 01-first-app/pages/people/index.js
@@ -0,0 +1,9 @@
+const People = () => {
+  return (
+    <div>
+      <h1>People Page</h1>
+    </div>
+  );
+};
+
+export default People;
---
+++ 01-first-app/pages/people/list.js
@@ -0,0 +1,8 @@
+function PeopleList() {
+  return (
+    <div>
+      <h1>People List</h1>
+    </div>
+  );
+}
+export default PeopleList;
