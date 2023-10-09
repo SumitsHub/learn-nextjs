d58cb27fb55e1fe1636b93d43f09fc2db255196a
Sumit Paul <pauls1317@gmail.com>
2023-10-09 23:05:39 +0530

    first-app: extracting nested dynamic path segment data

--- 01-first-app/pages/people/[id].js
+++ 01-first-app/pages/people/[id].js
@@ -1,4 +1,10 @@
+import { useRouter } from 'next/router';
+
 export default function () {
+  const router = useRouter();
+  console.log(router.pathname);
+  console.log(router.query); // {id: '1'}
+
   return (
     <div>
       <h1>Person</h1>
