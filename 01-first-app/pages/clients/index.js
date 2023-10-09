6cb3fce2d6455fc4379fc2a816284a4ca1bb5044
Sumit Paul <pauls1317@gmail.com>
2023-10-09 23:26:27 +0530

    first-app: Navigating with the Link Component from next/link

--- 01-first-app/pages/clients/index.js
+++ 01-first-app/pages/clients/index.js
@@ -1,4 +1,24 @@
+import Link from 'next/link';
+
+const clients = [
+  { id: 'max', name: 'Maxmilian' },
+  { id: 'manu', name: 'Manuel' },
+];
 function ClientsPage() {
-  return <div>Clients Home Page</div>;
+  return (
+    <div>
+      <h1>Clients Home Page</h1>
+
+      <ul>
+        {clients.map(client => {
+          return (
+            <li key={client.name}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 export default ClientsPage;
--- 01-first-app/pages/index.js
+++ 01-first-app/pages/index.js
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+
 function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
+      <ul>
+        <li>
+          {/* <a href="/about">About</a> */}
+          {/* Dont use simle a tag, it will make brand new HTTP request and everything(states etc) will be lost */}
+          <Link replace href="/about">
+            About
+          </Link>
+          {/* replace -> replace the page don't push, so can't go back */}
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
