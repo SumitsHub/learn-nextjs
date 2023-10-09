a261758a48cf9c3ef36cc106a2b288cbb82265d0
Sumit Paul <pauls1317@gmail.com>
2023-10-09 23:35:31 +0530

    first-app: router.push

--- 01-first-app/pages/clients/[id]/index.js
+++ 01-first-app/pages/clients/[id]/index.js
@@ -1,4 +1,22 @@
+import { useRouter } from 'next/router';
+
 function Client() {
-  return <div>Single Client</div>;
+  const router = useRouter();
+  const loadProject = () => {
+    // ...
+
+    // router.push(`/clients/max/123`);
+    // router.replace(`/clients/max/123`);
+    router.push({
+      pathname: `/clients/[id]/[projectId]`,
+      query: { id: 'max', projectId: '123' },
+    });
+  };
+  return (
+    <div>
+      <h1>Single Client</h1>
+      <button onClick={loadProject}>Go to project</button>
+    </div>
+  );
 }
 export default Client;
