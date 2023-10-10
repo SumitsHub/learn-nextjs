f1c9aa60f6ec0367d36a4c31fe91aee45169d22f
Sumit Paul <pauls1317@gmail.com>
2023-10-10 12:11:46 +0530

    events-app: result title

--- 02-events-app/pages/events/[...slug].js
+++ 02-events-app/pages/events/[...slug].js
@@ -1,4 +1,6 @@
+import Button from '@/components/common/Button';
 import EventList from '@/components/events/EventList';
+import ResultsTitle from '@/components/events/ResultTitle';
 import { getFilteredEvents } from '@/data/dummy';
 import { useRouter } from 'next/router';
 
@@ -25,19 +27,36 @@ function FilteredEvents() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <h4 className="center">Invalid Filters</h4>;
+    return (
+      <>
+        <h4 className="center">Invalid Filters</h4>;
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
   const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <h4>No events found based on filter</h4>;
+    return (
+      <>
+        <h4>No events found based on filter</h4>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
   }
 
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
+    <>
+      <ResultsTitle date={date} />
       <EventList events={filteredEvents} />
-    </div>
+    </>
   );
 }
 export default FilteredEvents;
