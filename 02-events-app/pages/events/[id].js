079715f232e198887f00d1105d004a69ff44108c
Sumit Paul <pauls1317@gmail.com>
2023-10-10 11:18:41 +0530

    events-app: using event-detail components

--- 02-events-app/pages/events/[id].js
+++ 02-events-app/pages/events/[id].js
@@ -1,4 +1,32 @@
+import EventContent from '@/components/event-detail/event-content';
+import EventLogistics from '@/components/event-detail/event-logistics';
+import EventSummary from '@/components/event-detail/event-summary';
+import { getEventById } from '@/data/dummy';
+import { useRouter } from 'next/router';
+
 function EventDetail() {
-  return <div>EventDetail Page</div>;
+  const router = useRouter();
+
+  const { id: eventId } = router.query;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <h4>No event found!</h4>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
 export default EventDetail;
