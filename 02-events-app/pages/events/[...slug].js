44b2d501adf107876273cd87ab1ba949383c6de4
Sumit Paul <pauls1317@gmail.com>
2023-10-10 12:02:01 +0530

    events-app: filtering logic

--- 02-events-app/pages/events/[...slug].js
+++ 02-events-app/pages/events/[...slug].js
@@ -1,4 +1,43 @@
+import EventList from '@/components/events/EventList';
+import { getFilteredEvents } from '@/data/dummy';
+import { useRouter } from 'next/router';
+
 function FilteredEvents() {
-  return <div>FilteredEvents</div>;
+  const router = useRouter();
+  const filterData = router.query.slug;
+  console.log(filterData);
+
+  if (!filterData) {
+    return <h4 className="center">Loading...</h4>;
+  }
+
+  const filterYear = filterData[0];
+  const filterMonth = filterData[1];
+
+  const numYear = +filterYear,
+    numMonth = +filterMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <h4 className="center">Invalid Filters</h4>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <h4>No events found based on filter</h4>;
+  }
+
+  return (
+    <div>
+      <EventList events={filteredEvents} />
+    </div>
+  );
 }
 export default FilteredEvents;
