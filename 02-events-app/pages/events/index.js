6396b511f25ff3a6ba8fd73ca4c2f78703404007
Sumit Paul <pauls1317@gmail.com>
2023-10-10 11:51:45 +0530

    events-app: navigating to filtered events page

--- 02-events-app/pages/events/index.js
+++ 02-events-app/pages/events/index.js
@@ -1,12 +1,17 @@
 import EventList from '@/components/events/EventList';
 import EventSearch from '@/components/events/EventSearch';
 import { getAllEvents } from '@/data/dummy';
+import { useRouter } from 'next/router';
 
 function Events() {
   const events = getAllEvents();
+  const router = useRouter();
+  const findEvents = (year, month) => {
+    router.push(`/events/${year}/${month}`);
+  };
   return (
     <>
-      <EventSearch />
+      <EventSearch onSearch={findEvents} />
       <EventList events={events} />
     </>
   );
